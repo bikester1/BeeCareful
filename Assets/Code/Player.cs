779ec4a9117f616fc53759e158ff97248ab722f1
bikester1 <cohen7424@hotmail.com>
2020-04-17 22:54:09 -0400

    Player movement
    left right up down

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,22 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Transform transform;
     CapsuleCollider collisionCap;
+    Camera cam;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform = GetComponent<Transform>();
-        collisionCap = transform.GetChild(1).GetComponent<CapsuleCollider>();
+        collisionCap = GetComponentInChildren<CapsuleCollider>();
+        cam = this.GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Left"))
+        ProcessInput();
+    }
+
+
+    void ProcessInput()
+    {
+        #region Horizontal Movement
+        if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Left");
+            this.transform.position += cam.transform.rotation * Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += cam.transform.rotation * Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += cam.transform.rotation * Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += cam.transform.rotation * Vector3.back * speed * Time.deltaTime;
+        }
+        #endregion
+
+        #region Vetical Movement
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            this.transform.position += cam.transform.rotation * Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.transform.position += cam.transform.rotation * Vector3.up * speed * Time.deltaTime;
+        }
+        #endregion
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log(cam.transform.rotation.eulerAngles);
         }
     }
 }
