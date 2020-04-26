7f4aa7c01559a7e1781872cf5d838c8c41708782
SideEffeks <josephrmcdowell@gmail.com>
2020-04-26 16:49:09 -0400

    Hive spawn ability

--- Assets/Code/Hive.cs
+++ Assets/Code/Hive.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Hive : MonoBehaviour
 {
+    public PrefabManager prefab;
+    private GameObject newBee;
+    private Vector3 spawnPosition;
+    public float spawnOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPosition = transform.rotation * Vector3.forward * spawnOffset;
+        spawnPosition += transform.position;
+        SpawnBee();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnBee()
+    {
+        newBee = Instantiate(prefab.bee);
+        newBee.GetComponent<Transform>().position = spawnPosition;
     }
 }
